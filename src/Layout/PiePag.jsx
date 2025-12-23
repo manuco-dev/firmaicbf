f7faf6120920ba3bf72c5cbbb314218d38e7e3da
Manuco <comanuel7@gmail.com>
2025-12-23 08:38:35 -0500

    cambios pro

--- src/Layout/PiePag.jsx
+++ src/Layout/PiePag.jsx
@@ -4,7 +4,6 @@
 function PiePag() {
   return (
     <footer className="PiePag">
-      <p>No registramos ni almacenamos el contenido de las solicitudes que realizas en este Sitio Web - Developer Web: Manuel Cogollo</p>
     </footer>
   );
 }
